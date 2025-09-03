e1d1f26e972445fcfd19d19b79880585d358678d
Agata Martinez <agatapetramali@gmail.com>
2025-09-03 14:07:24 -0300

    created controller views in index

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,23 +1,26 @@
 class ItinerariesController < ApplicationController
+
   def index
+    @itineraries = Itinerary.all
   end
 
   def show
   end
 
   def new
+
   end
 
   def create
+
   end
 
   def destroy
+
   end
 
   private
 
-  def itineraries_params
-  end
+  def
 
-  
 end
