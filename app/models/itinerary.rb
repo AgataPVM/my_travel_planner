7de02ba1f39ca04a04eca9ed8bad69feb21a1d5b
Karina Ferraz <ferrazgkarina@gmail.com>
2025-09-04 17:02:39 -0300

    added dependent destroy to model

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,6 +1,6 @@
 class Itinerary < ApplicationRecord
   belongs_to :user
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   validates :title, :destination, :days, :people, :interest, presence: true
 end
